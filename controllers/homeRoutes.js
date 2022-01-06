f167de7600fcab9d4a4c5cb05013473ecfe29641
TJ Tahmassebi <tjtahmas@gmail.com>
2022-01-06 00:13:12 -0500

    working on displaying member info on landing page. Broken everything but we're getting somewhere

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -6,8 +6,10 @@ const withAuth = require('../utils/auth')
 router.get('/', withAuth, async (req, res) => {
 
     let groups = [];
+    let member = [];
 
     try {
+        // Identify which groups the user belongs to
         const userGroupData = await UserGroup.findAll({
             where: { user_id: req.session.user_id }
         });
@@ -16,20 +18,40 @@ router.get('/', withAuth, async (req, res) => {
             const groupData = [];
 
             for (i = 0; i < userGroupData.length; i++) {
+                // Find data for each group the user belongs to
                 let newGroupData = await Group.findOne({
                     where: { id: userGroupData[i].group_id }
                 })
 
+                // Find user data for the user that matches the group's dungeon master id
+                // In order to display Dungeon Master name along with group
                 let dungeonMaster = await User.findOne({
                     where: { id: newGroupData.dun_master_id }
                 })
-
                 newGroupData.dataValues.dungeonMaster = dungeonMaster.user_name;
 
+                // Find all users for each group the user belongs to
+                // In order to display member information along with group
+                let newUserGroupData = await UserGroup.findAll({
+                    where: { group_id: newGroupData.id}
+                })
+                // Find data for each user in groups the user belongs to
+                
+                for (i = 0; i < newUserGroupData.length; i++){
+                    let newGroupUserData = await User.findOne({
+                        where: { id: newUserGroupData[i].user_id }
+                    })
+                    member[i] = newGroupUserData;
+                }
+                newGroupData.dataValues.member = member;
+                newGroupData.dataValues.members = member.length;
+                
                 groupData[i] = newGroupData;
             };
 
             groups = groupData.map((project) => project.get({ plain: true }));
+
+            console.log(groups)
 
         }
 
